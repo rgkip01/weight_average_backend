56bb1f5c0e219ad593bb602b0b7a80985c62c371
Rafael Abreu <web.nogs@gmail.com>
2024-02-16 02:31:14 -0300

    testing(avaliacao): Teste Model Avaliacao

--- spec/models/avaliacao_spec.rb
+++ spec/models/avaliacao_spec.rb
@@ -3,5 +3,41 @@
 require 'rails_helper'
 
 RSpec.describe Avaliacao, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:projeto) }
+  it { is_expected.to have_many(:notas).dependent(:destroy) }
+
+  describe 'callbacks' do
+    let(:avaliacao) { create(:avaliacao) }
+
+    it 'atualizar_media_ponderada após salvar' do
+      allow(avaliacao).to receive(:atualizar_media_ponderada)
+      avaliacao.run_callbacks(:save)
+      expect(avaliacao).to have_received(:atualizar_media_ponderada)
+    end
+  end
+
+  describe '#atualizar_media_ponderada' do
+    let(:avaliacao) { create(:avaliacao) }
+    let(:servico) { instance_double(CalculoMediaService) }
+
+    before do
+      allow(CalculoMediaService).to receive(:new).with(avaliacao: avaliacao).and_return(servico)
+    end
+
+    it 'atualiza a media_ponderada com sucesso' do
+      allow(servico).to receive(:calcular_media_ponderada).and_return(7.5)
+      avaliacao.send(:atualizar_media_ponderada)
+      expect(avaliacao.media_ponderada).to eq(7.5)
+    end
+
+    it 'não atualiza media_ponderada se o cálculo falhar' do
+      allow(servico).to receive(:calcular_media_ponderada).and_raise(StandardError)
+
+      expect do
+        avaliacao.send(:atualizar_media_ponderada)
+      rescue StandardError
+        StandardError
+      end.not_to change(avaliacao, :media_ponderada)
+    end
+  end
 end
