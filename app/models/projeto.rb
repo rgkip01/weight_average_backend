c8674bd08878e2cf283634033dab463855a963b7
Rafael Abreu <web.nogs@gmail.com>
2024-02-15 22:18:37 -0300

    refactor(models): refatoracao nas models para chamada do Service

--- app/models/projeto.rb
+++ app/models/projeto.rb
@@ -1,4 +1,13 @@
 class Projeto < ApplicationRecord
   has_many :avaliacoes, dependent: :destroy
   accepts_nested_attributes_for :avaliacoes
+
+  after_save :atualizar_media_total
+
+  private
+
+  def atualizar_media_total
+    servico = CalculoMediaService.new(projeto: self)
+    self.update_column(:media_total, servico.calcular_media_total)
+  end
 end
