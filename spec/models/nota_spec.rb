7467f794ba1aeac724d1f59baddd344a22b55723
Rafael Abreu <web.nogs@gmail.com>
2024-02-16 03:00:37 -0300

    testing(nota): Model Nota

--- spec/models/nota_spec.rb
+++ spec/models/nota_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe Nota, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:avaliacao) }
+  it { is_expected.to belong_to(:criterio) }
+
+  describe 'validações' do
+    it 'é válida com uma nota, avaliacao e criterio' do
+      nota = build(:nota)
+      expect(nota).to be_valid
+    end
+
+    it 'é inválida sem uma avaliacao' do
+      nota = build(:nota, avaliacao: nil)
+      expect(nota).not_to be_valid
+    end
+
+    it 'é inválida sem um criterio' do
+      nota = build(:nota, criterio: nil)
+      expect(nota).not_to be_valid
+    end
+  end
 end
