20a36f12f5044acb046193908db2af432535f941
Rafael Abreu <web.nogs@gmail.com>
2024-02-15 23:51:40 -0300

    refactor: remove espaços

--- app/services/calculo_media_service.rb
+++ app/services/calculo_media_service.rb
@@ -8,13 +8,16 @@ class CalculoMediaService
 
   def calcular_media_ponderada
     return 0 unless @avaliacao&.notas&.any?
+
     total_peso = @avaliacao.notas.joins(:criterio).sum('criterios.peso')
     return 0 if total_peso.zero?
+
     (@avaliacao.notas.joins(:criterio).sum('notas.nota * criterios.peso') / total_peso).round(2)
   end
 
   def calcular_media_total
     return 0 unless @projeto&.avaliacoes&.any?
+
     total = @projeto.avaliacoes.sum do |avaliacao|
       new_instance = self.class.new(avaliacao: avaliacao)
       new_instance.calcular_media_ponderada
@@ -22,4 +25,3 @@ class CalculoMediaService
     (total / @projeto.avaliacoes.count).round(2)
   end
 end
-
